ab62d7368cbfdf612ff06ceb5e50ec84ff16385b
nishantkabra123 <nishantkabra123@gmail.com>
2020-11-24 01:36:45 +0530

    Added:Heads or Tails shown as winner

--- FlipCoinSimulation.sh
+++ FlipCoinSimulation.sh
@@ -1,1 +1,12 @@
-echo welcome
+HEADS=1
+TAILS=0
+
+toss=$((RANDOM%2))
+
+if [ $toss -eq $TAILS ]
+then
+    echo Tails is winner
+elif [ $toss -eq $HEADS ]
+then
+    echo Heads is winner
+fi        
