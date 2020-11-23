a9fd06d59ad369a181ac18b84b2400e763acdf80
nishantkabra123 <nishantkabra123@gmail.com>
2020-11-24 01:43:35 +0530

    Refractored: Now showing multiple flip results

--- FlipCoinSimulation.sh
+++ FlipCoinSimulation.sh
@@ -1,12 +1,23 @@
 HEADS=1
 TAILS=0
+count=0
+countHead=0
+countTail=0
+while [ $count -ne 10 ]
+do
+    toss=$((RANDOM%2))
 
-toss=$((RANDOM%2))
+    if [ $toss -eq $TAILS ]
+    then
+        ((countTail++))
+        echo Tails is winner
+    elif [ $toss -eq $HEADS ]
+    then
+        ((countHead++))
+        echo Heads is winner
+    fi 
+    ((count++))
+done           
 
-if [ $toss -eq $TAILS ]
-then
-    echo Tails is winner
-elif [ $toss -eq $HEADS ]
-then
-    echo Heads is winner
-fi        
+echo Heads has won $countHead times
+echo Tails has won $countTail times
